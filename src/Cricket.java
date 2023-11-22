5e464985042c960d1f3c3e1520fac5d194d5a2dd
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2023-11-22 10:57:19 +0530

    initial

---
+++ src/Cricket.java
@@ -0,0 +1,24 @@
+public class Cricket extends Game {
+    @Override
+    void endPlay(){
+        System.out.println("Cricket game finished");
+
+    }
+
+    @Override
+    void initialize(){
+
+        System.out.println("Cricket game initialize Start Game");
+
+    }
+
+    @Override
+    void startPlay(){
+
+        System.out.println("Cricket Game Started .Enjoy the game");
+
+    }
+
+
+
+}
